687e30a1ac143bce80057216be173bc19c153e71
Bryant Richards <dragonseyeimages@gmail.com>
2019-10-31 19:39:53 -0500

    Stubbing out basic forms

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,30 @@
 class RecipesController < ApplicationController
+
+  get "/recipes" do
+    @recipes = Recipe.all
+    erb :"recipes/index"
+  end
+
+  get "/recipes/new" do
+    @users = User.all
+    erb :"recipes/new"
+  end
+
+  get "/recipes/:id" do
+    id = params[:id]
+    @recipe = Recipe.find_by_id(:id)
+    erb :"recipes/show"
+  end
+
+  post "/recipes" do
+    recipe = Recipe.new(params)
+    if recipe.save
+      redirect "recipes/#{recipe.id}"
+    else
+      redirect "recipes/new"
+    end
+
+  end
+
+
 end
