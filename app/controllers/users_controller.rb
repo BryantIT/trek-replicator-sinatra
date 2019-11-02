bd25746722736416adf13a8a0b1730d19636c3a6
Bryant Richards <dragonseyeimages@gmail.com>
2019-11-02 17:29:46 -0500

    Fixed some metaprogramming issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   post '/users' do
-    @user = User.new(params)
+    @user = User.new(username: params[:username], password: params[:password] )
     if @user.save
       session[:user_id] = @user.id
       redirect '/recipes'
